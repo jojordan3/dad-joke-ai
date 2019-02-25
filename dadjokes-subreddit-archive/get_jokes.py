9bf506610262ad16c62a0997238317f72ba0b687
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-02-24 22:07:08 -0800

    improve memory and time efficiency of gget_jokes.py

--- dadjokes-subreddit-archive/get_jokes.py
+++ dadjokes-subreddit-archive/get_jokes.py
@@ -2,7 +2,6 @@ import re
 import pandas as pd
 import json
 import os
-import time
 import sys
 
 
@@ -29,24 +28,23 @@ import sys
 #    return i, progress
 
 
-def filter_jokes(data):
+def filter_jokes(data, n_jokes):
     r_dadjokes = []
 
-    tracker_l = len(data)
     i = 0
     progress = 0
     width = 55
 
-    sys.stdout.write(f"Step {step} Progress: [>{' ' * (width - 1)}] 0.0%")
+    sys.stdout.write(f"Step 2 Progress: [>{' ' * (width - 1)}] 0.0%")
     sys.stdout.flush()
     sys.stdout.write('\b' * 4)
 
     while data:
-        current = int(width * i / tracker_l)
-        percent = i * 100 / tracker_l
+        current = int(width * i / n_jokes)
+        percent = i * 100 / n_jokes
         if progress < current:
             sys.stdout.write('\r')
-            sys.stdout.write(f"Step {step} Progress: \
+            sys.stdout.write(f"Step 2 Progress: \
 [{'=' * progress}>{' ' * (width - progress - 1)}] {percent:.1f}%")
             sys.stdout.flush()
             progress = current
@@ -56,21 +54,17 @@ def filter_jokes(data):
         sys.stdout.write('\b' * len(f'{percent:.1f}%'))
         i += 1
 
-        submission = data.pop(0)
-
-        if 't5_2t0no' in submission:
-            joke = json.loads(submission)
-            if joke['selftext']:
-                if 'https://' in joke['selftext']:
-                    continue
-                else:
-                    a = re.sub(r'\w* edit.*', '', joke['selftext'],
-                               flags=re.I | re.M)
-                    submission = f'Q: {joke["title"]}\nA: {a}'
-            else:
-                submission = str(joke['title'])
-            r_dadjokes.append([joke['id'], submission, joke['score'],
-                               joke['num_comments'], joke['created_utc']])
+        joke = json.loads(data.pop(0))
+        if 'https://' in joke['selftext']:
+            continue
+        elif joke['selftext']:
+            a = re.sub(r'\w* edit.*', '', joke['selftext'],
+                       flags=re.I | re.M)
+            submission = f'Q: {joke["title"]}\nA: {a}'
+        else:
+            submission = str(joke['title'])
+        r_dadjokes.append([joke['id'], submission, joke['score'],
+                           joke['num_comments'], joke['created_utc']])
 
     del data
 
@@ -81,29 +75,26 @@ if __name__ == "__main__":
     filename = input('filename: ')
     dadjokes = 'dadjokes.csv'
     with open(filename, 'r') as f:
-        data = f.readlines()
+        data = [line for line in f if 't5_2t0no' in line]
 
-    print("""
-Step 1: Read File -- Completed
+    n_jokes = len(data)
+
+    print(f"""
+Step 1: Extract Jokes -- Completed
+    with {n_jokes} found
 ----------------------------------------""")
     os.remove(filename)
 
-    r_dadjokes = filter_jokes(data)
-    n_jokes = len(r_dadjokes)
+    r_dadjokes = filter_jokes(data, n_jokes)
 
-    print(f"""\nStep 2: Filter Jokes -- Completed
-    with {n_jokes} found
+    print(f"""\nConfiguring Jokes -- Completed
 ----------------------------------------\n""")
-    sys.stdout.flush()
 
     dadjoke_df = pd.DataFrame(data=r_dadjokes,
                               columns=['id', 'joke', 'score', 'num_comments',
                                        'created']).set_index('id')
 
     del r_dadjokes
-
-    print("""\n   Configure Dad Jokes -- Completed
-----------------------------------------""")
 
     before = os.path.getsize(dadjokes)
 
