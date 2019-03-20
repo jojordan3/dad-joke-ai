2d813f1410646928a22e54375c4e27d7d5f18207
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-03-19 18:07:58 -0700

    update scrape_data.py

--- scrape_data.py
+++ scrape_data.py
@@ -4,22 +4,31 @@ import re
 from lxml import html
 
 
-base_url = 'https://icanhazdadjoke.com/search'
-r = requests.get(base_url)
+def get_icanhaz():
+    base_url = 'https://icanhazdadjoke.com/search'
+    r = requests.get(base_url)
 
-last_page = 0
+    last_page = 0
 
-soup = bsoup(r.content, 'lxml')
-# Get page numbers in search
-for a in soup.findAll('a', class_='pagination-link '):
-    last_page = max(last_page, int(a.text))
-
-
-dad_jokes = []
-
-for i in list(range(1, last_page + 1)):
-    url = base_url + f'?term=&page={i}'
-    r = requests.get(url)
     soup = bsoup(r.content, 'lxml')
-    for pre in soup.findAll('pre', style=True):
-        dad_jokes.append(pre.text)
+    # Get page numbers in search
+    for a in soup.findAll('a', class_='pagination-link '):
+        last_page = max(last_page, int(a.text))
+
+    dad_jokes = []
+
+    for i in list(range(1, last_page + 1)):
+        url = base_url + f'?term=&page={i}'
+        r = requests.get(url)
+        soup = bsoup(r.content, 'lxml')
+        for pre in soup.findAll('pre', style=True):
+            dad_jokes.append(pre.text)
+
+    return dad_jokes
+
+if __name__ == "__main__":
+    dj = get_icanhaz()
+
+    with open('icanhaz.csv', 'a') as f:
+        for line in dj:
+            f.write(line)
