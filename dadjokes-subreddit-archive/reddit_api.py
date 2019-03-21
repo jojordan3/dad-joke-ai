71636986e94a55439162b382d65af0423c5641df
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-03-21 01:27:45 -0700

    use reddit_api

--- dadjokes-subreddit-archive/reddit_api.py
+++ dadjokes-subreddit-archive/reddit_api.py
@@ -1,62 +1,76 @@
 import praw
 import time
 import sys
-
+import os
 
 data_col = ['id', 'created_utc', 'title', 'selftext', 'score', 'upvote_ratio',
             'gilded', 'over_18', 'num_comments']
 gildings = ['gid_1', 'gid_2', 'gid_3']
-r = praw.Reddit()
+r = praw.Reddit('')
 
 
 def write_joke(submission, jokes):
-    medals = submission.gildings
-    author = submission.author
-    sub_vars = vars(submission)
-    data = [sub_vars[i] if i in sub_vars else '' for i in data_col]
-    data.append(author.name)
-    data.extend([medals[g] for g in gildings])
-    jokes.write(str(data)[1:-1] + '\n')
+    try:
+        medals = submission.gildings
+        author = submission.author
+        sub_vars = vars(submission)
+        data = [sub_vars[i] if i in sub_vars else '' for i in data_col]
+        try:
+            data.append(author.name)
+        except AttributeError:
+            data.append('')
+        data.extend([medals[g] for g in gildings])
+        jokes.write(str(data)[1:-1] + '\n')
+    except:
+        print(f'\nError processing t3_{submission.id}\n')
+        raise
 
 
-def next_batch(how='after', last=None):
+def next_batch(how='before', last=None):
     batch = r.subreddit('dadjokes').new(params={how: last})
+    orig = os.path.getsize('/Users/joannejordan/Desktop/GitHub/dad-joke-ai/dadjokes-subreddit-archive/rdadjokes.csv')
     with open('rdadjokes.csv', 'a') as jokes, open('records.txt',
                                                    'a') as records:
         i = 0
-        width = 55
-        progress = 0
-        sys.stdout.write(f"Progress: [>{' ' * (width - 1)}] 0%")
-        sys.stdout.flush()
-        sys.stdout.write('\b' * 2)
-
         for submission in batch:
-            current = int(width * i / 100)
-            if progress < current:
-                sys.stdout.write('\r')
-                sys.stdout.write(f"Progress: \
-[{'=' * progress}>{' ' * (width - progress - 1)}] {i}%")
-                sys.stdout.flush()
-                progress = current
-            else:
-                sys.stdout.write(f'{i}%')
-                sys.stdout.flush()
-            sys.stdout.write('\b' * len(f'{i}%'))
-
             if i == 0:
-                records.write(submission.id + ' to ')
-            elif i == 99:
-                last = submission.id
-                records.write(last + '\n')
+                records.write(f't3_{submission.id} to ')
+            else:
+                last = f't3_{submission.id}'
             write_joke(submission, jokes)
             i += 1
+            time.sleep(1)
+        new = os.path.getsize('/Users/joannejordan/Desktop/GitHub/dad-joke-ai/dadjokes-subreddit-archive/rdadjokes.csv')
+        if new == orig:
+            raise ValueError('Nothing added')
+        records.write(last + '\n')
     return last
 
-# LIMIT TO 1 TODO
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        last = next_batch(how=sys.argv[1], last=sys.argv[2])
-    else:
-        last = next_batch()
-    print(f'Recorded up to {last}')
+    error = False
+    batch_ = 1
+    while not error:
+        if batch_ == 1:
+            try:
+                if len(sys.argv) > 1:
+                    last = next_batch(how=sys.argv[1], last=sys.argv[2])
+                else:
+                    last = next_batch(how='before', last=last)
+                batch_ += 1
+            except:
+                print('Error in first batch')
+                raise
+        else:
+            try:
+                last = next_batch(how='after', last=last)
+                print(
+                    f'Recorded batch#{batch_} with last submission: {last}\n')
+                batch_ += 1
+            except KeyboardInterrupt:
+                print(f'\nRecorded up to {last}\n')
+                error = True
+            except:
+                error = True
+                print(f'\nError processing batch {batch_}\n')
+                raise
