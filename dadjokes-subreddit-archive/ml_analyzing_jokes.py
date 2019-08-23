3798abc4f0b442a248aa734694965ebe650bfd5b
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-08-22 23:15:59 -0700

    previous change to analyzing jokes

--- dadjokes-subreddit-archive/ml_analyzing_jokes.py
+++ dadjokes-subreddit-archive/ml_analyzing_jokes.py
@@ -1,6 +1,12 @@
 '''Analyzing reddit jokes for likes, comments, medals'''
 import numpy as np
 import pandas as pd
-# from sklearn import 
+# from sklearn import
 
 
+csv_path = '/Users/joannejordan/Desktop/GitHub/dad-joke-ai/dadjokes-subreddit-\
+archive/pushshift.csv'
+
+dj = pd.read_csv(csv_path)
+dj.drop(columns=['NA'], inplace=True)
+dj.apply()
